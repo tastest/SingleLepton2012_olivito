4117c0f2cb8b3d0c25d9fdc1b1f07caeffc9fc9b
olivito <olivito>
2013-06-24 12:48:58 +0000

    parallelizing scripts

---
+++ macros/WHLooper/do.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OUTDIR=V24_cr5invmass
+
+declare -a Samples=(ttsl ttdl tsl tdl wjets wbb others data)
+
+mkdir -p output/${OUTDIR}
+
+for SAMPLE in ${Samples[@]};
+  do echo root -b -q -l doAll.C\(\"${SAMPLE}\",\"${OUTDIR}\"\)
+  nohup root -b -q -l doAll.C\(\"${SAMPLE}\",\"${OUTDIR}\"\) > log_${SAMPLE}.txt 2<&1 &
+done
--- macros/WHLooper/doAll.C
+++ macros/WHLooper/doAll.C
@@ -8,7 +8,7 @@
 #include "WHLooper.h"
 #endif
 
-void doAll() {
+void doAll(std::string runsample = "", std::string outdir = "") {
 
   gSystem->Load("libTree.so");
   gSystem->Load("libPhysics.so");
@@ -17,6 +17,7 @@ void doAll() {
 
   gSystem->Load("../../Tools/MiniFWLite/libMiniFWLite.so");
 
+  gROOT->ProcessLine(".L libWHLooperCORE.so");
   gROOT->ProcessLine(".L libWHLooper.so");
 
   WHLooper *looper = new WHLooper();
@@ -25,52 +26,137 @@ void doAll() {
   // samples to run over
   //
  
-  std::string indir = "/nfs-3/userdata/stop/output_V00-02-11_2012_2jskim";
+  //  std::string indir = "/nfs-3/userdata/stop/output_V00-02-20_2012_2jskim";
+  //  std::string indir = "/nfs-7/userdata/stop/output_V00-02-20_2012_2jskim";
+  //  std::string indir = "/nfs-3/userdata/stop/output_V00-02-20_2012_4jskim";
+  // std::string indir = "/nfs-7/userdata/stop/output_V00-02-21_2012";
+  //  std::string indir = "/nfs-7/userdata/stop/output_V00-02-21_2012_2jskim";
+  //  std::string indir = "/nfs-7/userdata/stop/output_V00-02-24_2012";
+  std::string indir = "/nfs-7/userdata/stop/output_V00-02-24_2012_2jskim";
+  //  std::string indir = "/nfs-7/userdata/stop/output_V00-02-27_2012_2jskim";
+  //  std::string indir = "/nfs-7/userdata/stop/output_V00-02-28_2012";
 
-  std::string outdir = "sigsel_pfmet150_bbpt150_njets2";
+  if (outdir.size() == 0) {
+    std::cout << "ERROR: must specify outdir, exiting" << std::endl;
+    exit();
+  }
+
+  //  std::string outdir = "V20_wjetscomp_nb012";
+  //  std::string outdir = "V20_stopcomp_tauveto";
+  //  std::string outdir = "V20_dilflavors_nfs7";
+  //  std::string outdir = "V20_metdphiplots";
+  //  std::string outdir = "V21_wjetscomp_reshaping";
+  //  std::string outdir = "V21_leadjetpt50_preselmt50";
+  //  std::string outdir = "V21_leadjetpt50_cr9";
+  //  std::string outdir = "V21_incpresel";
+  //  std::string outdir = "V24_cr5invmass";
+  //  std::string outdir = "V27_ttsl_jetacc_pumvamed_nmissed";
+  //  std::string outdir = "V28_ttsl_tobtectest";
+
 
   TChain* data        = new TChain("t");
   TChain* ttfake        = new TChain("t");
   TChain* ttsl        = new TChain("t");
   TChain* ttdl        = new TChain("t");
+  TChain* ttsl_mg        = new TChain("t");
+  TChain* ttdl_mg        = new TChain("t");
   TChain* wjets       = new TChain("t");
+  TChain* wbb         = new TChain("t");
   TChain* zjets       = new TChain("t");
   TChain* vv          = new TChain("t");
-  TChain* t           = new TChain("t");
+  TChain* tsl         = new TChain("t");
+  TChain* tdl         = new TChain("t");
   TChain* ttV         = new TChain("t");
   TChain* vvv         = new TChain("t");
+  TChain* whbb        = new TChain("t");
+  TChain* tchiwh_125_1         = new TChain("t");
+  TChain* tchiwh_150_1         = new TChain("t");
+  TChain* tchiwh_200_1         = new TChain("t");
   TChain* tchiwh_250_1         = new TChain("t");
+  TChain* tchiwh_250_25        = new TChain("t");
+  TChain* tchiwh_250_50        = new TChain("t");
+  TChain* tchiwh_300_1         = new TChain("t");
   TChain* tchiwh_350_1         = new TChain("t");
+  TChain* tchiwh_400_1         = new TChain("t");
 
   std::vector<string> labels;
   std::vector<TChain*> sample;
 
   data->  Add(Form("%s/data_*.root"       , indir.c_str()));
-  wjets-> Add(Form("%s/w1to4jets.root"         , indir.c_str()));
-  zjets-> Add(Form("%s/DYStitchtot.root"         , indir.c_str()));
+  //  data->  Add(Form("%s/data_*.root"       , "/nfs-3/userdata/stop/output_V00-02-16_2012_2jskim" ));
+  wjets-> Add(Form("%s/w1to4jets*.root"         , indir.c_str()));
+  //  wjets-> Add(Form("%s/w1to4jets*.root"         , "/nfs-7/userdata/stop/output_V00-02-21_2012_2jskim"));
+  // wjets-> Add(Form("%s/w1to4jets*.root"         , "/nfs-3/userdata/stop/output_V00-02-20_2012"));
+  // wbb-> Add(Form("%s/wbbjets*.root"         , indir.c_str()));
+  wbb-> Add(Form("%s/wbbjets*.root"         , "/nfs-7/userdata/stop/output_V00-02-21_2012_2jskim"));
+  // wbb-> Add(Form("%s/wbbjets*.root"         , "/nfs-3/userdata/stop/output_V00-02-20_2012"));
+  //  zjets-> Add(Form("%s/DYStitchtot.root"         , indir.c_str()));
+  zjets-> Add(Form("%s/DY1to4Jtot.root"         , indir.c_str()));
   ttfake->    Add(Form("%s/ttfake_powheg.root"         , indir.c_str()));
   ttsl->    Add(Form("%s/ttsl_powheg.root"         , indir.c_str()));
   ttdl->    Add(Form("%s/ttdl_powheg.root"         , indir.c_str()));
+  // ttsl_mg->    Add(Form("%s/ttsl_lmg.root"         , indir.c_str()));
+  ttsl_mg->    Add(Form("%s/ttsl_lmgtau*.root"         , indir.c_str()));
+  // ttdl_mg->    Add(Form("%s/ttdl_lmg.root"         , indir.c_str()));
+  ttdl_mg->    Add(Form("%s/ttdl_lmgtau*.root"         , indir.c_str()));
   vv->    Add(Form("%s/diboson.root"            , indir.c_str()));
-  t->     Add(Form("%s/tWall.root"             , indir.c_str()));
+  tsl->     Add(Form("%s/tW_lepsl.root"             , indir.c_str()));
+  tdl->     Add(Form("%s/tW_lepdl.root"             , indir.c_str()));
+  //  t->     Add(Form("%s/tWall_lep.root"             , indir.c_str()));
   ttV->   Add(Form("%s/ttV.root"           , indir.c_str()));
   vvv->   Add(Form("%s/triboson.root"           , indir.c_str()));
+  whbb->   Add(Form("%s/whbb.root"           , indir.c_str()));
+  //  whbb->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-20/whbb.root");
 
-  tchiwh_250_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-17/TChiwh_250_1.root");
-  tchiwh_350_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-17/TChiwh_350_1.root");
+  //  tchiwh_125_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_125_1.root");
+  tchiwh_150_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_150_1.root");
+  tchiwh_200_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_200_1.root");
+  tchiwh_250_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_250_1.root");
+  tchiwh_250_25->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_250_25.root");
+  tchiwh_250_50->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_250_50.root");
+  tchiwh_300_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_300_1.root");
+  tchiwh_350_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_350_1.root");
+  tchiwh_400_1->Add("/home/users/olivito/SingleLepton2012/looper/output/V00-02-28/TChiwh_400_1.root");
 
-  sample.push_back(ttfake);  labels.push_back("ttbar_0l");
-  sample.push_back(ttsl);    labels.push_back("ttbar_1l");
-  sample.push_back(ttdl);    labels.push_back("ttbar_2l");
-  sample.push_back(wjets);   labels.push_back("wjets");
-  sample.push_back(zjets);   labels.push_back("zjets");
-  sample.push_back(vv);      labels.push_back("VV");
-  sample.push_back(t);       labels.push_back("single_top");
-  sample.push_back(ttV);     labels.push_back("ttV");
-  sample.push_back(vvv);     labels.push_back("VVV");
-  sample.push_back(tchiwh_250_1);     labels.push_back("TChiwh_250_1");
-  sample.push_back(tchiwh_350_1);     labels.push_back("TChiwh_350_1");
-  //  sample.push_back(data);    labels.push_back("data");
+  if (runsample == "ttsl") {
+    sample.push_back(ttsl_mg);    labels.push_back("ttbar_mg_1l");
+  } else if (runsample == "ttdl") {
+    sample.push_back(ttdl_mg);    labels.push_back("ttbar_mg_2l");
+  } else if (runsample == "tsl") {
+    sample.push_back(tsl);       labels.push_back("single_top_1l");
+  } else if (runsample == "tdl") {
+    sample.push_back(tdl);       labels.push_back("single_top_2l");
+  } else if (runsample == "wjets") {
+    sample.push_back(wjets);   labels.push_back("wjets_nobb");
+  } else if (runsample == "wbb") {
+    sample.push_back(wjets);   labels.push_back("wjets_onlybb");
+    sample.push_back(wbb);   labels.push_back("wbb");
+  } else if (runsample == "others") {
+    sample.push_back(zjets);   labels.push_back("zjets");
+    sample.push_back(vv);      labels.push_back("VV");
+    sample.push_back(ttV);     labels.push_back("ttV");
+    sample.push_back(vvv);     labels.push_back("VVV");
+    sample.push_back(whbb);    labels.push_back("whbb");
+    sample.push_back(tchiwh_150_1);     labels.push_back("TChiwh_150_1");
+    sample.push_back(tchiwh_200_1);     labels.push_back("TChiwh_200_1");
+    sample.push_back(tchiwh_250_1);     labels.push_back("TChiwh_250_1");
+    sample.push_back(tchiwh_250_25);    labels.push_back("TChiwh_250_25");
+    sample.push_back(tchiwh_250_50);    labels.push_back("TChiwh_250_50");
+    sample.push_back(tchiwh_300_1);     labels.push_back("TChiwh_300_1");
+    sample.push_back(tchiwh_350_1);     labels.push_back("TChiwh_350_1");
+    sample.push_back(tchiwh_400_1);     labels.push_back("TChiwh_400_1");
+  } else if (runsample == "data") {
+    sample.push_back(data);    labels.push_back("data");
+  } else {
+    std::cout << "ERROR: didn't recognize runsample: " << runsample << ", exiting" << std::endl;
+    exit();
+  }
+
+  // sample.push_back(ttfake);  labels.push_back("ttbar_0l");
+  // sample.push_back(ttsl);    labels.push_back("ttbar_1l");
+  // sample.push_back(ttdl);    labels.push_back("ttbar_2l");
+  // sample.push_back(t);       labels.push_back("single_top");
+  // sample.push_back(tchiwh_125_1);     labels.push_back("TChiwh_125_1");
 
   for (unsigned int i = 0; i < sample.size(); ++i) {
     std::cout << "running on sample: " << labels[i] << std::endl;
