34848a88e4cab92e98dee842c7f2609de36976ca
olivito <olivito>
2013-06-26 08:45:40 +0000

    simpler version using Draw

--- scripts/scan_nsig.C
+++ scripts/scan_nsig.C
@@ -1,4 +1,4 @@
-void scan_nsig(TString dir, TString output, int xmin, int xmax, int xstep, int ymin = -1, int ymax = -1, int ystep = -1, int delta = -1) {
+void scan_nsig(TString dir, TString output, bool do2d = true) {
 
   TChain* t = new TChain("Events");
   t->Add(dir + "/merged*.root");
@@ -6,33 +6,17 @@ void scan_nsig(TString dir, TString output, int xmin, int xmax, int xstep, int y
 
   TFile* fout = new TFile(output,"RECREATE");
 
-  int xbins = (xmax - xmin)/xstep + 1;
-  // 1D scan: only loop over x values
-  if (ymin < 0) {
+  // 1D scan
+  if (!do2d) {
 
     TH1F* h_masses = new TH1F("masses","", 21, 0., 525.);
+    t->Draw("sparm_values[0]>>masses");
 
-    for (int ix = 0; ix < xbins; ++ix) {
-      int xval = ix * xstep + xmin;
-      int n = t->GetEntries(Form("sparm_values[0] == %d",xval));
-      h_masses->SetBinContent(ix + xmin/xstep + 1, n);
-    }
-
-  // 2D scan: loop over x and y values
+  // 2D scan
   } else {
 
     TH2F* h_masses = new TH2F("masses","", 21, 0., 525., 21, 0., 525.);
-    int ybins = (ymax - ymin)/ystep + 1;
-
-    for (int ix = 0; ix < xbins; ++ix) {
-      int xval = ix * xstep + xmin;
-      for (int iy = 0; iy < ybins; ++iy) {
-	int yval = iy * ystep + ymin;
-	if (xval - yval < delta) continue;
-	int n = t->GetEntries(Form("sparm_values[0] == %d && sparm_values[1] == %d",xval,yval));
-        h_masses->SetBinContent(ix + xmin/xstep + 1, iy + ymin/ystep + 1, n);
-      }
-    }
+    t->Draw("sparm_values[1]:sparm_values[0]>>masses");
 
   }
 
