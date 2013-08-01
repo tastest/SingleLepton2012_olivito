579f3281320aa0b3693b7561292717673c2b2713
olivito <olivito>
2013-08-01 13:29:52 +0000

    latest script

--- macros/WHLooper/do.sh
+++ macros/WHLooper/do.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-OUTDIR=V24_cr14nomt
+OUTDIR=V24_crs_blinded
 
-declare -a Samples=(ttsl ttdl tsl tdl wjets wbb others data)
+#declare -a Samples=(ttsl ttdl tsl tdl wjets wbb others tchiwh data wino ttsl_powheg ttdl_powheg)
+declare -a Samples=(ttsl ttdl tsl tdl wjets wbb others data wino)
+#declare -a Samples=(ttsl ttdl tsl tdl wjets wbb others tchiwh wino)
+#declare -a Samples=(ttsl_powheg ttdl_powheg)
+#declare -a Samples=(ttsl ttdl)
+#declare -a Samples=(ttsl wjets)
+#declare -a Samples=(wzlight)
+#declare -a Samples=(wzbb)
+#declare -a Samples=(whbb)
+#declare -a Samples=(wbb)
+#declare -a Samples=(wjets others)
+#declare -a Samples=(data)
+#declare -a Samples=(others)
+#declare -a Samples=(hhwwbb)
+#declare -a Samples=(tchiwh wino)
+#declare -a Samples=(wino)
+#declare -a Samples=(ttv)
+#declare -a Samples=(scan)
 
 mkdir -p output/${OUTDIR}
 
